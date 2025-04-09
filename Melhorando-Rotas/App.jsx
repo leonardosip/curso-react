edd0fe40112bbbf2a37a981f3e636d5274f706a7
leonardosip <leonarddopereira2004@gmail.com>
2025-04-09 02:09:59 -0300

    melhorando em rotas

---
+++ Melhorando-Rotas/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import RoutesApp from './routes';
+import Header from './components/Header';
+
+
+function App() {
+
+  
+  return (
+    <>
+    <RoutesApp/>
+    </>
+    
+  );
+}
+
+export default App;
